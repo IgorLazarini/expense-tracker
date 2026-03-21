18f723c38fac90138b710e6ce34bdc712640ca1b
Igor Lazarini <lazariniigor7@gmail.com>
2026-03-21 14:20:31 -0300

    delete sincronizando com localStorage

--- script.js
+++ script.js
@@ -5,12 +5,32 @@ const inputCateg = document.getElementById("Categoria");
 const lista = document.getElementById("listaGastos");
 const totalgastos = document.getElementById("total");
 let gtotal = 0;
-let gastos = {
-    descricao:
-    valor:
-    categoria:
-};
-form.addEventListener("submit", function(event) {
+let gastos = JSON.parse(localStorage.getItem("gastos")) || [];
+
+gastos.forEach(function(gasto){
+
+    const item = document.createElement ("li");
+    item.textContent = gasto.descricao + " - R$" + gasto.valor + " (" + gasto.categoria + ")";
+    const bExcluir = document.createElement("button");
+        bExcluir.textContent = "X";
+    bExcluir.addEventListener("click", function(){
+        item.remove();
+        gtotal -= gasto.valor;
+        gastos = gastos.filter(function(g) {
+            return g!== gasto;
+        })
+        localStorage.setItem("gastos", JSON.stringify(gastos));
+
+        totalgastos.textContent = gtotal;
+    })
+    item.appendChild(bExcluir);
+    lista.appendChild(item);
+
+    gtotal += gasto.valor;
+});
+    totalgastos.textContent = gtotal;
+
+    form.addEventListener("submit", function(event) {
     event.preventDefault();
 
     const descricao = inputDescricao.value;
@@ -24,12 +44,23 @@ form.addEventListener("submit", function(event) {
     bExcluir.addEventListener("click", function(){
         item.remove();
         gtotal -= valor;
-
+     gastos = gastos.filter(function(dg) {
+        return!(
+        dg.descricao === descricao &&
+        dg.valor === valor &&
+        dg.categoria === categoria
+        );
+    });
+    localStorage.setItem("gastos", JSON.stringify(gastos));
         totalgastos.textContent = gtotal;
     });
-        item.appendChild(bExcluir);
-        lista.appendChild(item);
-
+    gastos.push({
+        descricao: descricao,
+        valor: valor,
+        categoria: categoria
+    });
+    localStorage.setItem("gastos", JSON.stringify(gastos));
+    
     gtotal += valor;
 
     totalgastos.textContent = gtotal;
@@ -37,4 +68,7 @@ form.addEventListener("submit", function(event) {
     console.log(descricao);
     console.log(valor);
     console.log(categoria);
+    
+    item.appendChild(bExcluir);
+    lista.appendChild(item);
 });
